f07b572c65d40247a402b0f6d69c691f694f09d1
Kamal Parvez <kamal@cokreates.com>
2019-03-10 13:07:08 +0600

    Spring demo 1st Assignment completed

---
+++ demo-spring-assignment/src/ckl/kamal/assignment/CricketCoach.java
@@ -0,0 +1,10 @@
+package ckl.kamal.assignment;
+
+public class CricketCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice everyday for 2 hours.";
+	}
+
+}
---
+++ demo-spring-assignment/src/ckl/kamal/assignment/SpringDemoApp.java
@@ -0,0 +1,30 @@
+package ckl.kamal.assignment;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringDemoApp {
+
+	public static void main(String[] args) {
+
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+				
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCricketCoach", Coach.class);
+		
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// close the context
+		context.close();
+	}
+
+}
+
+
+
+
+
+
+
